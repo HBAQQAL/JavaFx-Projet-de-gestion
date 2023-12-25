0c49d3f31d8d0be34b4c751d140758f83328e01b
HBAQQAL <hamzabaqqal2002@gmail.com>
2023-12-25 06:47:41 +0100

    initial commit

---
+++ demo1/src/main/java/com/example/demo1/dao/dbConnection.java
@@ -0,0 +1,10 @@
+package com.example.demo1.dao;
+
+public class dbConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mydatabase";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+}
